4ae9e10ba8f5a3de03d0746aeca00f08a4c83901
yatesr <ryan@openmrs.org>
2013-11-07 16:59:46 -0500

    added updatedb script

--- roles/postsetup/files/updatedb.sh
+++ roles/postsetup/files/updatedb.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-touch /tmp/hi
+#!/bin/sh
+# cd to script directory TODO: Change to work with different versions
+cd /opt/openempi-2.2.7/conf/
+# Create database
+psql openempi < create_new_database_schema-2.2.0.sql
+psql openempi < update_database_schema-2.2.1.sql
+psql openempi < update_database_schema-2.2.3.sql
+psql openempi < update_database_schema-2.2.4.sql
+psql openempi < update_database_schema-2.2.6.sql
+psql openempi < update_database_schema-2.2.7.sql
